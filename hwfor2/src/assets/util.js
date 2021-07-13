1c53a4453bf903d6e3c3b64644f06c6a227073b3
yuenayem <yunusemremeydanci@hotmail.com>
2021-07-14 02:15:09 +0300

    util.js created for currency symbol matching

---
+++ hwfor2/src/assets/util.js
@@ -0,0 +1,24 @@
+const currency_symbols = {
+    'USD': '$', // US Dollar
+    'EUR': '€', // Euro
+    'CRC': '₡', // Costa Rican Colón
+    'GBP': '£', // British Pound Sterling
+    'ILS': '₪', // Israeli New Sheqel
+    'INR': '₹', // Indian Rupee
+    'JPY': '¥', // Japanese Yen
+    'KRW': '₩', // South Korean Won
+    'NGN': '₦', // Nigerian Naira
+    'PHP': '₱', // Philippine Peso
+    'PLN': 'zł', // Polish Zloty
+    'PYG': '₲', // Paraguayan Guarani
+    'THB': '฿', // Thai Baht
+    'UAH': '₴', // Ukrainian Hryvnia
+    'VND': '₫', // Vietnamese Dong
+    'TRY': '₺', // Vietnamese Dong
+};
+
+export default {
+    convertCurrencyToSymbol(value){
+        return currency_symbols[value] ? currency_symbols[value] : "asdas"
+    }
+}
