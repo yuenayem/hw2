070ae2e6dc134152b70cec537863797e99b2698e
yuenayem <yunusemremeydanci@hotmail.com>
2021-07-14 02:14:46 +0300

    necessary packages installed item component is created, app.vue updated

--- hwfor2/src/App.vue
+++ hwfor2/src/App.vue
@@ -1,27 +1,93 @@
 <template>
   <div id="app">
-    <list></list>
+    <header class="header">
+      <section class="brand-title">Hürriyet Emlak</section>
+      <aside class="aside">
+        <button @click="navigateToCart" class="cart-button">
+          <span>My Cart</span>
+          <span class="item-amount"> {{this.$store.state.itemsInCart.length}}</span>
+        </button>
+      </aside>
+    </header>
+    <main class="main">
+      <router-view></router-view>
+    </main>
+    <footer></footer>
   </div>
 </template>
 
 <script>
-import List from './components/List.vue'
-
 export default {
-  name: 'App',
-  components: {
-    List
+  name: "App",
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {
+    navigateToCart() {
+      this.$router.push({ path: "basket" });
+    }
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
+$red-orange: #fb641b;
+
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+  .header {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 80px;
+    background: #fb641b00;
+    box-shadow: 0px 0px 5px 0px rgb(0 0 0 / 75%);
+
+    .brand-title {
+      color: #fb641b;
+      font-weight: 900;
+      font-size: 28px;
+      text-shadow: 1px 1px 1px #00000061;
+    }
+    .aside {
+      .cart-button {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        background: $red-orange;
+        border: 0;
+        color: #ffffff;
+        width: 100px;
+        font-size: 14px;
+        padding: 5px;
+        border-radius: 4px;
+        cursor: pointer;
+
+        &:hover {
+          background: transparentize($red-orange, 0.2);
+        }
+
+        .item-amount {
+          background: $red-orange;
+          padding: 0px 4px;
+          border-radius: 5px;
+          color: white;
+        }
+      }
+    }
+  }
+
+  .main {
+  }
 }
 </style>
--- hwfor2/src/components/Item.vue
+++ hwfor2/src/components/Item.vue
@@ -1,71 +1,117 @@
 <template>
-    <div class="item-main">
-        <div class="item-header">
-            <button class="operations">...</button>
-        </div>
-        <div class="item-body">
-            <div class="item-text">Design Landing Page For Support Team</div>
-        </div>
-        <div class="item-footer">
-            <div class="profile">
-                <div class="profile-image">
-                    ad
-                </div>
-                <div class="profile-name">
-                    asdasdasd
-                </div>
-            </div>
-            <div class="date">
-                asdasdas
-            </div>
-        </div>
+  <div class="item-main">
+    <div class="item-header">
+      <img :src="item.image" :alt="item.name" />
     </div>
+    <div class="item-body">
+      <div class="item-text">{{item.name}}</div>
+    </div>
+    <div class="item-footer">
+      <div class="pricing-container">
+        <span class="currency">{{util.convertCurrencyToSymbol(item.currency)}}</span>
+        <span class="price">{{item.price}}</span>
+      </div>
+      <button @click="addBasket(item)" class="add-basket">
+        ADD BASKET
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
+import { UPDATE_CART_ITEMS } from "../store/mutation.types";
+import Util from '../assets/util'
 export default {
   name: "Item",
   props: {
-    text: String
+    item: Object
+  },
+  data() {
+    return {
+      util: Util,
+      menuVisible: false
+    };
+  },
+  methods: {
+    addBasket(item) {
+      let itemsInCart = this.$store.state.itemsInCart;
+      let foundIndex = itemsInCart.findIndex(element => element.id === item.id);
+      if (foundIndex === -1) {
+        item.amount = 0;
+        itemsInCart.push(item);
+        this.$store.commit(UPDATE_CART_ITEMS, itemsInCart);
+        this.$router.push({ path: "basket" });
+      } else {
+        itemsInCart[foundIndex].amount++;
+        this.$store.commit(UPDATE_CART_ITEMS, itemsInCart);
+        this.$router.push({ path: "basket" });
+      }
+    }
   }
 };
 </script>
 
 
 <style lang="scss" scoped>
+$red-orange: #fb641b;
+
 .item-main {
-  border-radius: 5px;
+  border: 1px solid #0000000f;
+  width: 230px;
+  height: 240px;
   background: #ffffff;
+  padding: 10px;
   .item-header {
     display: flex;
-    justify-content: flex-end;
+    justify-content: center;
+    align-items: center;
     width: 100%;
-    height: 10px;
+    position: relative;
+    padding: 6px;
 
-    .operations {
-          margin: 0;
-    border: 0;
-    background: none;
-    font-weight: 900;
-    height: initial;
-    padding: 0;
-    line-height: 0;
+    img {
+      width: 150px;
+      height: 150px;
     }
   }
+
   .item-body {
-      .item-text {
-          font-weight: 600;
-          text-align: left;
-      }
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 15px 0;
+    .item-text {
+      text-align: left;
+    }
   }
 
   .item-footer {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 12px;
+    .pricing-container {
       display: flex;
-      justify-content: space-between;
-  font-size: 12px;
-      .profile {
-        display: flex;
-      }
+      justify-content: center;
+      font-weight: 600;
+      color: transparentize(#000000, 0.2);
+      font-size: 14px;
+    }
+
+    .add-basket {
+      background: $red-orange;
+      color: #ffffff;
+      border: 0;
+      padding: 4px;
+      width: 104px;
+      height: 32px;
+      border-radius: 4px;
+      cursor: pointer;
+
+       &:hover {
+          background: transparentize($red-orange, 0.2);
+        }
+    }
   }
 }
 </style>
