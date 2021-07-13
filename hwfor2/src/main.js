50613eab24ee593df4f569d42313329a005f4eef
yuenayem <yunusemremeydanci@hotmail.com>
2021-07-14 02:15:30 +0300

    main.js router and vuex set up

--- hwfor2/src/main.js
+++ hwfor2/src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Routes from './router/index.js'
+import Vuex from 'vuex'
+import state from './store'
 
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+  routes: Routes
+})
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: "#app",
+  router: router,
+  store: state,
+  render: h => h(App)
+})
